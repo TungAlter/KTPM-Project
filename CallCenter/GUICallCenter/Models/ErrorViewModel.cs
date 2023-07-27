d3e9ad7b41e3a6e039d956efc3d18bacadacde7e
Lumifa1110 <lmphat20@clc.fitus.edu.vn>
2023-07-27 21:36:55 +0700

    new CallCenter structure

--- CallCenter/GUICallCenter/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace GUICallCenter.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
