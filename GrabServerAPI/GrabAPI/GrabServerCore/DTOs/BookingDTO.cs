a5297f98227e20a27b101bac6e4511d0478a4ce1
Lumifa1110 <lmphat20@clc.fitus.edu.vn>
2023-08-14 21:47:25 +0700

    update Home S2

---
+++ GrabServerAPI/GrabAPI/GrabServerCore/DTOs/BookingDTO.cs
@@ -0,0 +1,10 @@
+namespace GrabServerCore.DTOs {
+    public class BookingDTO {
+        public int IdBooking { get; set;};
+        public int IdCustomer { get; set;};
+        public string SrcAddress { get; set;};
+        public string DesAddress { get; set;};
+        
+       
+    }
+}
