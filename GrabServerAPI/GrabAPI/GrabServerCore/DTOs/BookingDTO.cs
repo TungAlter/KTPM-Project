a14d26be06f2706daed16e1215afd99377f5395f
Lumifa1110 <lmphat20@clc.fitus.edu.vn>
2023-08-15 14:25:38 +0700

    add lib

--- GrabServerAPI/GrabAPI/GrabServerCore/DTOs/BookingDTO.cs
+++ GrabServerAPI/GrabAPI/GrabServerCore/DTOs/BookingDTO.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace GrabServerCore.DTOs {
     public class BookingDTO {
         public int IdBooking { get; set;}
