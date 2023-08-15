5752aca59d308c03b6830517304ebeec182937f2
Lumifa1110 <lmphat20@clc.fitus.edu.vn>
2023-08-15 14:24:40 +0700

    update mock data show card

--- GrabServerAPI/GrabAPI/GrabServerCore/DTOs/Booking/AddBookingDTO.cs
+++ GrabServerAPI/GrabAPI/GrabServerCore/DTOs/Booking/AddBookingDTO.cs
@@ -9,15 +9,15 @@ namespace GrabServerCore.DTOs.Booking
     public class AddBookingDTO
     {
         public int IdCustomer { get; set; }
-        public DateTime DateBooking { get; set; }
-        public double SrcLong { get; set; }
-        public double SrcLat { get; set; }
+        // public DateTime DateBooking { get; set; }
+        // public double SrcLong { get; set; }
+        // public double SrcLat { get; set; }
         public string SrcAddress { get; set; } = string.Empty;
-        public double DesLong { get; set; }
-        public double DesLat { get; set; }
+        // public double DesLong { get; set; }
+        // public double DesLat { get; set; }
         public string DesAddress { get; set; } = string.Empty;
-        public double Distance { get; set; }
-        public string Notes { get; set; } = string.Empty;
-        public int Total { get; set; }
+    //     public double Distance { get; set; }
+    //     public string Notes { get; set; } = string.Empty;
+    //     public int Total { get; set; }
     }
 }
